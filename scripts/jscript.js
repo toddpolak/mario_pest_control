c9fe287dcae9eba4b506115698c01e3185024057
toddpolak <67641770+toddpolak@users.noreply.github.com>
2020-09-30 20:29:06 -0700

    JS Setup

--- scripts/jscript.js
+++ scripts/jscript.js
@@ -1,1 +1,7 @@
-// JavaScript file for mario_pest_control
+const baddieForm = document.baddies;
+
+baddieForm.addEventListener('submit', event => {
+    event.preventDefault()
+    let goombaQty = baddieForm.goombas.value;
+    console.log(goombaQty);
+});
